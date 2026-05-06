1f01069363b99c2ba91cbb005de0fe7598faf0d2
Maria Elena Aisha Garcia Rodriguez <aishagarcia05@gmail.com>
2026-05-06 08:38:52 -0600

    primer commit

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+int main () {
+    int edad;
+
+    printf("Hola, bienvenido al programa!!!! \n");
+    printf("Ingresa tu edad: ");
+    scanf("%f", &edad);
+
+    if(edad>=18){
+        printf("\nEres mayor de edad");
+    
+    }
+    else {
+        printf("\nAun no eres mayor de edad");
+    }
+
+    return 0;
+}
